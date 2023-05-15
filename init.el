3dd0da4ae4b225af8a9a624ca3aa002c8eedfbd4
Harry Gao <beatbox_gao@hotmail.com>
2023-05-15 22:34:38 +0800

    modify emacs

--- init.el
+++ init.el
@@ -22,13 +22,16 @@
   (straight-use-package-by-default t))
 
 (use-package evil
+  :init
+  (setq evil-want-C-i-jump nil)
   :config
   (evil-mode))
 
 (use-package cider
   :config
   (add-hook 'clojure-mode-hook #'cider-mode)
-  (setq tab-always-indent 'complete))
+  ;;(setq tab-always-indent 'complete)
+  )
 
 (use-package smartparens
   :config
@@ -60,20 +63,44 @@
   :config
   (dashboard-setup-startup-hook))
 
+(use-package helm
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x C-f" . helm-find-files)))
+
+(use-package bing-dict
+  :bind
+  (("C-c d" . bing-dict-brief))
+  :config
+  (setq bing-dict-add-to-kill-ring t))
+
 (use-package org-bullets
   :config
   (add-hook 'org-mode-hook #'org-bullets-mode))
 
 (add-hook 'org-mode-hook 'visual-line-mode)
 (add-hook 'org-mode-hook 'org-indent-mode)
+(setq org-directory "~/Dropbox/org")
+(setq note-file (concat org-directory "/notes.org"))
+(setq org-default-notes-file note-file)
+
+(defun org-go-to-note()
+  (interactive)
+  (find-file note-file))
+
+;; (ensure-install 'nano-theme)
+;; (load-theme 'nano-light)
 
 ;; (set-frame-font "Sarasa Mono SC Nerd 18" nil t)
 
 (setq font-size
       (if IS-MAC 18 36))
-(set-face-attribute 'default nil :font (font-spec :family "Sarasa Mono SC Nerd" :size font-size))
+(setq font-name
+      (if IS-MAC "Sarasa Mono SC Nerd" "Sarasa Term SC Nerd"))
+(set-face-attribute 'default nil :font (font-spec :family font-name :size font-size))
 
-(tool-bar-mode -1)
+(tool-bar-mode -1) ;; disable mac menu bar
+(menu-bar-mode -1) ;; disable linux menu bar
 
 (global-display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
