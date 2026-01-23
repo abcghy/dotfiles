21f76502e62e599700fe63bfce71a8f5ab0dd96c
Harry Gao <beatbox_gao@hotmail.com>
2026-01-23 18:59:32 +0800

    add fnm and ssh-gpg support for fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,4 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# GPG Agent for SSH Authentication in Fish Shell
+
+# Erase the standard SSH_AGENT_PID to avoid conflicts
+set -e SSH_AGENT_PID
+
+# Check if pinentry has already set the socket for us
+if set -q gnupg_SSH_AUTH_SOCK_by_pinentry
+    set -gx SSH_AUTH_SOCK "$gnupg_SSH_AUTH_SOCK_by_pinentry"
+else
+    # If not, find the socket location using gpgconf
+    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+end
+
+# Update the TTY for pinentry to work correctly in the current terminal
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
 zoxide init fish | source
